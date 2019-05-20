44125f52beb334159ac2577c6d0ebe5686471840
Max Lipp <info@maxlipp.com>
2019-05-20 14:01:36 +0200

    Awesome Dickscord

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,5 +1,11 @@
 <template>
-     <h1>This is Events page</h1>
+<v-container>
+     <v-layout>
+          <v-flex xs12>
+               <iframe src="https://discordapp.com/widget?id=577441083848654849&theme=dark" height="500px" allowtransparency="true" frameborder="0"></iframe>
+          </v-flex>
+     </v-layout>
+</v-container>
 </template>
 
 <script>
