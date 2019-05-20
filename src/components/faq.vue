61b3cd9c857398962fd7af536ecdf3f4fde0ac63
tutaru99 <43951824+SimasRau@users.noreply.github.com>
2019-05-20 21:25:44 +0300

    Pricing page + faq

---
+++ src/components/faq.vue
@@ -0,0 +1,22 @@
+<template>
+     <v-container>
+        <v-layout row>
+            <v-flex xs12 md6 lg3 xl3>
+            <h1>Frequently asked questions</h1>
+            </v-flex>
+        </v-layout>
+     </v-container>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
+
+
