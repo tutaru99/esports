b025d683b8baf1b33f909b5783e626150555aaae
Max Lipp <info@maxlipp.com>
2019-05-20 16:01:29 +0200

    changes have been made

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,19 +1,49 @@
 <template>
-<v-container>
-     <v-layout>
-          <v-flex xs12>
-               <iframe src="https://discordapp.com/widget?id=577441083848654849&theme=dark" height="500px" allowtransparency="true" frameborder="0"></iframe>
-          </v-flex>
-     </v-layout>
-</v-container>
+  <v-container>
+    <v-layout>
+      <v-flex >
+        <v-flex
+          class="fb-page"
+          data-href="https://www.facebook.com/eSportEsbjerg/"
+          data-tabs="events,timeline"
+          data-width
+          data-height
+          data-small-header="false"
+          data-adapt-container-width="true"
+          data-hide-cover="false"
+          data-show-facepile="true"
+        >
+          <blockquote cite="https://www.facebook.com/eSportEsbjerg/" class="fb-xfbml-parse-ignore">
+            <a href="https://www.facebook.com/eSportEsbjerg/">eSport Esbjerg</a>
+          </blockquote>
+        </v-flex>
+        <div
+          class="fb-page"
+          data-href="https://www.facebook.com/EsbjergLanParty/"
+          data-tabs="events"
+          data-width
+          data-height
+          data-small-header="false"
+          data-adapt-container-width="true"
+          data-hide-cover="false"
+          data-show-facepile="true"
+        >
+          <blockquote
+            cite="https://www.facebook.com/EsbjergLanParty/"
+            class="fb-xfbml-parse-ignore"
+          >
+            <a href="https://www.facebook.com/EsbjergLanParty/">Esbjerg Lan Party - ELP</a>
+          </blockquote>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
-<style scoped>
 
+<style scoped>
 </style>
--- src/views/News.vue
+++ src/views/News.vue
@@ -1,5 +1,16 @@
 <template>
-     <h1>This is News page</h1>
+<v-container>
+     <v-layout>
+    <v-flex xs12>
+        <iframe
+          src="https://discordapp.com/widget?id=577441083848654849&theme=dark"
+          height="500px"
+          allowtransparency="true"
+          frameborder="0"
+        ></iframe>
+      </v-flex>
+     </v-layout>
+</v-container>
 </template>
 
 <script>
