04eee8e0b177b484bd05e3bba1667b89f955a95e
tutaru99 <43951824+SimasRau@users.noreply.github.com>
2019-05-20 17:42:33 +0300

    did somethings, pc crashed, cant remember

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,12 @@
  <template>
+<<<<<<< HEAD
         <v-toolbar app flat height="100">
+            <router-link to="/" class="ma-3" >
+=======
+       <v-toolbar app flat height="100">
+          
             <router-link to="/" class="ma-3">
+>>>>>>> a2eedc056181bf2e5806ed76c4f418114642a0c8
                 <img src="../assets/logo.png" alt="Logo" height="137px" class="mt-5">
             </router-link>
             <v-toolbar-title>
@@ -12,7 +18,42 @@
             </router-link>
             <router-link to="/Events" class="ma-3">
                 Events
-            </router-link>                
+<<<<<<< HEAD
+            </router-link>
+=======
+            </router-link> 
+            <div class="navbar">
+  <div class="dropdown">
+    <button class="dropbtn">GAMES 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <v-container fluid class="dropdown-content">
+      <v-layout align-center class="row">
+        <v-flex >
+          <h3>GAMES WE OFFER</h3>
+          <router-link to="/Events" class="ma-3">
+            <v-img height="151px" width="268px" :src="require('./../assets/fortnite.png')">
+            </v-img>
+          </router-link>
+           <router-link to="/Events" class="ma-3">
+            <v-img height="151px" width="268px" :src="require('./../assets/cs.png')">
+            </v-img>
+          </router-link>
+           <router-link to="/Events" class="ma-3">
+            <v-img height="151px" width="268px" :src="require('./../assets/ow.png')">
+            </v-img>
+          </router-link>
+           <router-link to="/Events" class="ma-3">
+            <v-img height="151px" width="268px" :src="require('./../assets/fifa.png')">
+            </v-img>
+          </router-link>  
+        </v-flex>
+       
+      </v-layout>
+    </v-container>
+  </div> 
+</div>              
+>>>>>>> a2eedc056181bf2e5806ed76c4f418114642a0c8
             <router-link to="/Games" class="ma-3">
                 Games
             </router-link>         
@@ -29,14 +70,14 @@
     <script>
   export default {
     data: () => ({
-      items: [
-        'All', 'Family', 'Friends', 'Coworkers'
-      ]
+     
     })
   }
+  
 </script>
     
     <style scoped>
+    
 *{
   font-family: 'Open Sans', sans-serif;
 }
@@ -53,5 +94,97 @@
 background-color:transparent;
 }
 
+/* Navbar container */
+.navbar {
+  overflow: hidden;
+}
+
+/* Links inside the navbar */
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* The dropdown container */
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+/* Dropdown button */
+.dropdown .dropbtn {
+  font-size: 16px; 
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font: inherit; /* Important for vertical align on mobile phones */
+  margin: 0; /* Important for vertical align on mobile phones */
+}
+
+/* Add a red background color to navbar links on hover */
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: #2c93c6;
+}
+
+/* Dropdown content (hidden by default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #2F333D;
+  width: 100%;
+  left: 0;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Mega Menu header, if needed */
+.dropdown-content .header {
+  background: red;
+  padding: 16px;
+  color: white;
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+/* Create three equal columns that floats next to each other
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 10px;
+  background-color: #ccc;
+  height: 250px;
+}
+ */
+
+/* Style links inside the columns */
+.column a {
+  float: none;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+/* Add a background color on hover */
+.column a:hover {
+  background-color: #ddd;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+} 
     </style>
     
--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -1,6 +1,13 @@
 <template>
- 
-    <h1>This is Pricing page</h1>
+        <v-container id="BgPreview">
+            <v-layout align-center justify-start row fill-height >
+                <v-flex xs12 md7 lg5 xl4 >
+                    <h1 class="mb-4">Our Pricing Models</h1>
+                  
+                  
+                </v-flex>
+            </v-layout>
+        </v-container>
   
 </template>
 
@@ -11,5 +18,10 @@ export default {
 </script>
 
 <style scoped>
-
+#BgPreview {
+    background-image: url("../assets/picture-1.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 600px;
+}
 </style>
