8cb0e42e0afb1e43c428dfcdfc8b783f7e4e8b66
Max Lipp <info@maxlipp.com>
2019-05-20 20:04:45 +0200

    Delete shit + crazy navbar

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,43 +1,5 @@
 <template>
-  <v-container>
-    <v-layout>
-      <v-flex >
-        <v-flex
-          class="fb-page"
-          data-href="https://www.facebook.com/eSportEsbjerg/"
-          data-tabs="events,timeline"
-          data-width
-          data-height
-          data-small-header="false"
-          data-adapt-container-width="true"
-          data-hide-cover="false"
-          data-show-facepile="true"
-        >
-          <blockquote cite="https://www.facebook.com/eSportEsbjerg/" class="fb-xfbml-parse-ignore">
-            <a href="https://www.facebook.com/eSportEsbjerg/">eSport Esbjerg</a>
-          </blockquote>
-        </v-flex>
-        <div
-          class="fb-page"
-          data-href="https://www.facebook.com/EsbjergLanParty/"
-          data-tabs="events"
-          data-width
-          data-height
-          data-small-header="false"
-          data-adapt-container-width="true"
-          data-hide-cover="false"
-          data-show-facepile="true"
-        >
-          <blockquote
-            cite="https://www.facebook.com/EsbjergLanParty/"
-            class="fb-xfbml-parse-ignore"
-          >
-            <a href="https://www.facebook.com/EsbjergLanParty/">Esbjerg Lan Party - ELP</a>
-          </blockquote>
-        </div>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  
 </template>
 
 <script>
