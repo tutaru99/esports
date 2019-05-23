fabeaa6574826fa8fe0f69705749dcd3c50c86cd
tutaru99 <43951824+SimasRau@users.noreply.github.com>
2019-05-23 11:38:26 +0300

    data slice for paragraph

--- src/components/data.vue
+++ src/components/data.vue
@@ -14,7 +14,7 @@
                     <v-img :src="wpitem.imgurl"  aspect-ratio="3" />
                 <div class="pa-4">
                     <h1 class="mt-3" v-html= "wpitem.title.rendered" />
-                    <p id="description" class="mt-2" v-html="wpitem.content.rendered.slice(0, 80) + ' ..'" />
+                    <p id="description" class="mt-2" v-html="wpitem.content.rendered.slice(0, 169) + '...'" />
                     </div>
                     <div>
                     <v-icon medium color="#4b4e58">query_builder</v-icon>
