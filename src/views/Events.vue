16946f3d78dbd98290f916bdae4538f4da3737cf
Max Lipp <info@maxlipp.com>
2019-05-27 12:27:14 +0200

    update events

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,19 +1,38 @@
 <template>
   <v-container>
-      <v-layout> <v-flex xs12 md7 lg7 xl4>
-                    <div class="mt-5" > <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FeSportEsbjerg%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500"
-                     style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
-                     <div class="mt-5"><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FEsbjergLanParty%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500"
-                      style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
-                </v-flex>
-      </v-layout>
+    <v-layout align-center justify-center>
+      <v-flex xs12 md7 lg7 xl4>
+        <div class="mt-5">
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FeSportEsbjerg%2F&tabs=events&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            width="320"
+            height="500"
+            style="border:none;overflow:hidden"
+            scrolling="no"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+        </div>
+        <div class="mt-5">
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FEsbjergLanParty%2F&tabs=events&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            width="320"
+            height="500"
+            style="border:none;overflow:hidden"
+            scrolling="no"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {
-    
-};
+export default {};
 </script>
 
 
