a70a853b00d17af4914fac8baae6b50e72bab7d8
tutaru99 <43951824+SimasRau@users.noreply.github.com>
2019-05-27 13:28:34 +0300

    deleted faq content for 4 games and about

--- src/views/OVERWATCH.vue
+++ src/views/OVERWATCH.vue
@@ -12,9 +12,13 @@
        <v-layout >
            <v-flex class="text" lg6>
                <h1  id="Headline" class="ml-5 mt-5 mb-3">
-                   KURWA BLYAT
+                   WHAT IS OVERWATCH?
                </h1>
-               <p id="pf" class="mb-5 ml-5">Wield unworldly power with Magic Leap One’s Control. An engineering marvel, it doesn’t require any additional external sensors to deliver six degrees of freedom, also known as 6DoF. Use your Control as a paint brush, a conductor’s baton or a weaponized ice cream cone that launches Rocky Road at 14 scoops per second.</p>
+               <p id="pf" class="mb-5 ml-5">Overwatch is a team-based multiplayer first-person shooter.
+                  Often described as a "hero shooter", 
+                 Overwatch assigns players into two teams of six, with each player selecting from a roster 
+                 of 31 characters, known as "heroes", 
+                 each with a unique style of play whose roles are divided into three general categories that fit their role. </p>
            </v-flex>
            <v-flex lg6>
                <v-img  class="img" :src="require('./../assets/owch.png')"></v-img>
@@ -23,18 +27,22 @@
    </v-container>
 
 <v-container grid-list-md>
-    <v-layout align-center justify-center row fill-height wrap>
+    <v-layout align-start justify-center row fill-height wrap>
       <v-flex d-flex xs12 sm6 md3 lg3 xl3>
           <div class="my-5 mx-2" color="transparent" dark>
           <h4>COMMUNICATION</h4>
-          <p class="mt-2">psum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alien</p>
+          <p class="mt-2">Overwatch has a built-in voice comms, as well as
+             chat-wheel with premade voices for every situation needed 
+            and if that is not enough - there is a ping system in place, so that players can ping locations they want.</p>
           </div>
       </v-flex>
 
         <v-flex  d-flex xs12 sm6 md3 lg3 xl3>
        <div class="my-5 mx-2" color="transparent" dark> 
           <h4>INGAME ECONOMY</h4>
-          <p class="mt-2">psum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alien</p>
+          <p class="mt-2">As you play games of Overwatch, you’ll gain levels, 
+            unlocking new customization options to make your heroes
+             look and sound unique! Customize your characters in the Hero Gallery</p>
        </div>
       </v-flex>
 
@@ -42,14 +50,19 @@
        <div class="my-5 mx-2" color="transparent" dark> 
           <h4>GAME AWARENESS</h4>
          
-          <p class="mt-2">psum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alien</p>
+          <p class="mt-2">There are many ways to play Overwatch. We recommend starting with the tutorial
+             to learn the fundamentals of play; once you’ve completed it, head to the Main Menu to try out several different 
+             game modes.</p>
        </div>
       </v-flex>
       
         <v-flex d-flex xs12 sm6 md3 lg3 xl3>
        <div class="my-5 mx-2" color="transparent" dark> 
           <h4>TACTICS AND STRATEGY</h4>
-          <p class="mt-2">psum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alien</p>
+          <p class="mt-2">When you enter a game of Overwatch, you'll need to choose a hero to play.
+Maybe a cannon-toting ape from the moon appeals to you. 
+Perhaps you prefer a time-traveling freedom fighter. Or is a beat-dropping battlefield DJ
+ more your style? Whoever you want to be, there's an Overwatch hero for you.</p>
        </div>
       </v-flex>
     </v-layout>
